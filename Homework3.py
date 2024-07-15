06496edd6671ef92130fb4e27dda6a8855dab853
ippopovis <144596156+ippopovis@users.noreply.github.com>
2024-07-15 12:50:48 +0300

    Add files via upload

---
+++ Homework3.py
@@ -0,0 +1,8 @@
+stroka_1 = 'это моя строка'
+stroka_2 = 'это новая строка'
+print(stroka_1[0])
+print(stroka_1[-1])
+print(stroka_1[2:6])
+print(stroka_1[::-1])
+print(len(stroka_1))
+print(stroka_2 + stroka_1)
